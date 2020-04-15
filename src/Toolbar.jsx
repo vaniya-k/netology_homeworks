7440508021cc83c227a56d22ff42a84ff57da424
vaniya <vaniya.karakasiyan@gmail.com>
2020-04-15 15:39:05 +0300

    2-1 done

---
+++ src/Toolbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const FilterItem = ({filterName, isActive, onFilterButtonClick}) => {
+  return (
+  <div className={`filter-item ${(isActive === true ? `filter-item__active` : null)}`} onClick={onFilterButtonClick}>{filterName}</div>
+  )
+}
+
+const Toolbar = ({filters, activeFilter, onFilterButtonClick}) => {
+  return(
+    <div className="filter-container">
+      {filters.map((filter, i) => 
+        <FilterItem
+          key={`key${i}`}
+          filterName={filter}
+          isActive={(filters.indexOf(filter) === activeFilter) ? true : false}
+          onFilterButtonClick={() => onFilterButtonClick(i)}
+        />
+      )}
+    </div>
+  )
+};
+
+export default Toolbar;
