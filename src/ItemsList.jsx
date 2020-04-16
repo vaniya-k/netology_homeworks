6bdf78803ddacc9c27a0e05589bdbeb70136553d
vaniya <vaniya.karakasiyan@gmail.com>
2020-04-16 20:10:50 +0300

    done

---
+++ src/ItemsList.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import CardsList from './CardsList.jsx';
+import StripesList from './StripesList.jsx';
+
+const LIST_TYPES = [`view_list`, `view_module`]
+
+const IconSwitch = ({activeListType, onIconClick}) => {
+ return (
+  <div className="view-icon">
+    <i className="material-icons md-48" onClick={() => onIconClick()}>
+      {LIST_TYPES[activeListType]}
+    </i>
+  </div>
+ )
+}
+
+class ItemsList extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      activeListType: 0
+    }
+
+    this.handleListTypeSwitch = this.handleListTypeSwitch.bind(this);
+  }
+
+  handleListTypeSwitch = () => {
+    let newValue = 0;
+    
+    if (this.state.activeListType === 0) {
+      newValue = 1;
+    }
+
+    this.setState({
+      activeListType: newValue
+    })
+  }
+
+  render() {
+    return <React.Fragment>
+      <IconSwitch activeListType={this.state.activeListType} onIconClick={this.handleListTypeSwitch}/>
+      {(this.state.activeListType === 0) ? <CardsList products={this.props.products}/> : <StripesList products={this.props.products}/>}
+    </React.Fragment>
+  }
+
+}
+
+export default ItemsList;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Portfolio from './Portfolio.jsx';
+import ItemsList from './ItemsList.jsx';
+
+const products = [{
+  name: "Nike Metcon 2",
+  price: "130",
+  color: "red",
+  img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/layouts/img/1.jpg"
+}, {
+  name: "Nike Metcon 2",
+  price: "130",
+  color: "green",
+  img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/layouts/img/2.jpg"
+}, {
+  name: "Nike Metcon 2",
+  price: "130",
+  color: "blue",
+  img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/layouts/img/3.jpg"
+}, {
+  name: "Nike Metcon 2",
+  price: "130",
+  color: "black",
+  img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/layouts/img/4.jpg"
+},{
+  name: "Nike Metcon 2",
+  price: "130",
+  color: "red",
+  img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/layouts/img/1.jpg"
+}, {
+  name: "Nike Metcon 2",
+  price: "130",
+  color: "green",
+  img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/layouts/img/2.jpg"
+}];
 
 ReactDOM.render(
-    <Portfolio />,
+    <ItemsList products={products}/>,
   document.getElementById('root')
 );
