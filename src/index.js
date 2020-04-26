b8747f1d674d19a3ca87079783a8ce096a9c908d
vaniya <vaniya.karakasiyan@gmail.com>
2020-04-26 16:27:20 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Stars from './Stars.jsx';
+import Listing from './Listing.jsx';
+import JSON_DATA from './mock-data.js';
+
+const jsonItemAdapter = (item) => {
+
+  const mainImage = item[`MainImage`]
+
+  return {
+    id: item.listing_id,
+    itemUrl: item.url,
+    imageUrl: mainImage[`url_570xN`],
+    title: item.title,
+    price: item.price,
+    currency: item.currency_code,
+    quantity: item.quantity,
+  }
+}
+
+const processedData = JSON_DATA.map(item => jsonItemAdapter(item));
 
 ReactDOM.render(
-    <Stars count={4}/>,
+    <Listing items={processedData}/>,
   document.getElementById('root')
 );
