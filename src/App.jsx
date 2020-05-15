34f7da618c92666a703f77cd544115c4e153c82a
vaniya <vaniya.karakasiyan@gmail.com>
2020-05-15 10:08:45 +0300

    Исправление замечаний

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <>
     <Card/>
-    <Card children={{img: `https://pbs.twimg.com/profile_images/3119746052/b84bbd1707018b90df39765df7a9ecee.jpeg`}}/>
+    <Card>{{img: `https://pbs.twimg.com/profile_images/3119746052/b84bbd1707018b90df39765df7a9ecee.jpeg`}}</Card>
     </>
   )
 };
