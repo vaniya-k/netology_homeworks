6a58cf2d9a26572cb0c87584f14047b55ca0f7ac
vaniya <vaniya.karakasiyan@gmail.com>
2020-05-19 08:44:55 +0300

    Внесение исправлений v2

--- src/TopStaffListingHighlight.jsx
+++ src/TopStaffListingHighlight.jsx
@@ -4,28 +4,21 @@ import {useState, useEffect} from 'react';
 const DetailsView = ({focusedId}) => {
   const [detailsDataSet, setDetailsDataSet] = useState([]);
 
-  const buildDetailsDataSet = (data) => {
-    const {id, name, details} = data;
-    const {city, position} = details;
-
-    setDetailsDataSet([`${name}`,`${city}`, `${position}`, id])
-  }
-  
   useEffect(() => {
     fetch(`https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/hooks-context/use-effect/data/${focusedId}.json`)
       .then(res => res.json())
-      .then(data => buildDetailsDataSet(data))
+      .then(data => setDetailsDataSet({id: data.id, name: data.name, city: data.details[`city`], position: data.details[`position`]}))
   },[focusedId]);
 
   return (
     <div className="staff-item-details">
-      {(focusedId !== detailsDataSet[3])
+      {(focusedId !== detailsDataSet.id)
         ? <h4>Loading...</h4>
         : <>
           <h4>Details</h4>
-          <span>{`Name: ${detailsDataSet[0]}`}</span>
-          <span>{`City: ${detailsDataSet[1]}`}</span>
-          <span>{`Position: ${detailsDataSet[2]}`}</span>
+          <span>{`Name: ${detailsDataSet.name}`}</span>
+          <span>{`City: ${detailsDataSet.city}`}</span>
+          <span>{`Position: ${detailsDataSet.position}`}</span>
         </>
       }
     </div>
